448e6228b52462d51b5e56a62d1bf2e31ac566f0
zangsu <zangsu_@naver.com>
2023-12-12 20:40:49 +0900

    refactor : 출력 문자 일부 상수화

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,6 +5,9 @@ import racingcar.domain.Cars;
 import racingcar.view.io.Printer;
 
 public class OutputView {
+    public static final String POSITION_MARK = "-";
+    public static final String ROUND_RESULT_FORMAT = "%s : %s";
+    public static final String WINNER_DELIMITER = ", ";
     private static final String EXCEPTION_PREFIX = "[ERROR] ";
 
     public static void printException(Exception e) {
@@ -21,13 +24,13 @@ public class OutputView {
 
     public static void printRoundResult(Cars cars) {
         cars.getCars()
-                .forEach(car -> Printer.printMessageUsingFormat("%s : %s",
+                .forEach(car -> Printer.printMessageUsingFormat(ROUND_RESULT_FORMAT,
                         car.getName(),
-                        "-".repeat(car.getPosition())));
+                        POSITION_MARK.repeat(car.getPosition())));
         newLine();
     }
 
     public static void printWinner(List<String> winners) {
-        Printer.printMessage("최종 우승자 : " + String.join(", ", winners));
+        Printer.printMessage("최종 우승자 : " + String.join(WINNER_DELIMITER, winners));
     }
 }
